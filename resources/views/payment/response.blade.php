5adca8dfb569b86370ca4cdc13f435640ad01d66
Rakesh Kumar <rakesh@dignitech.com>
2021-04-05 18:58:44 +0000

    Final update

---
+++ resources/views/payment/response.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+
+<div class="col-12 mt-5">
+    <div class="alert alert-info">
+       <h1> The payment status is: {{$transaction->transaction_status}}</h1>
+    </div>
+</div>
+</x-layout>
